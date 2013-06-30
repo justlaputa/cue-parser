898fcd06dcdf99ef05b7b0a8f30c8e0c6b11812f
justlaputa <justlaputa@gmail.com>
2013-06-30 14:54:33 +0800

    finish all parse functions

--- test/sample.cue
+++ test/sample.cue
@@ -1,14 +1,24 @@
 CATALOG 3898347789120
+CDTEXTFILE "C:\LONG FILENAME.CDT"
 PERFORMER "Sample performer"
+SONGWRITER "Sample songwriter"
 TITLE "Sample title"
+REM Comment in toplevel
 FILE "sample file.ape" WAVE
   TRACK 01 AUDIO
+    FLAGS DCP PRE
+    ISRC ABCDE1234567
     TITLE "Sample track 1"
     PERFORMER "Sample performer"
+    SONGWRITER "Sample songwriter"
+    PREGAP 00:02:00
     INDEX 00 00:00:00
     INDEX 01 00:00:33
+    POSTGAP 00:02:00
   TRACK 02 AUDIO
-    TITLE "Sample track 1"
+    TITLE "Sample track 2"
     PERFORMER "Sample performer"
+    SONGWRITER "Sample songwriter"
     INDEX 00 00:05:10
     INDEX 01 00:05:23
+    REM Comment in track
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var expect = require('expect.js')
-  , parser = require('../lib/cue');
+, parser = require('../lib/cue');
 
 describe('cue-parser', function() {
     var sheet;
@@ -13,7 +13,7 @@ describe('cue-parser', function() {
     });
 
     it('should parse CDTEXTFILE', function() {
-        expect(sheet.cdTextFile).to.be(null);
+        expect(sheet.cdTextFile).to.be('C:\\LONG FILENAME.CDT');
     });
 
     it('should parse FILE', function() {
@@ -23,35 +23,95 @@ describe('cue-parser', function() {
         expect(sheet.files[0].type).to.be('WAVE');
     });
 
+    it('should parse Disk PERFORMER', function() {
+        expect(sheet.performer).to.be('Sample performer');
+    });
+
+    it('should parse Disk SONGWRITER', function() {
+        expect(sheet.songWriter).to.be('Sample songwriter');
+    });
+
+    it('should parse Disk TITLE', function() {
+        expect(sheet.title).to.be('Sample title');
+    })
+
     it('should parse all tracks of the file', function() {
         var tracks = sheet.files[0].tracks;
-
 
         expect(tracks).to.be.an('array');
         expect(tracks).to.have.length(2);
 
-        expect(track[0].number).to.be(1);
-        expect(track[0].type).to.be('AUDIO');
+        expect(tracks[0].number).to.be(1);
+        expect(tracks[0].type).to.be('AUDIO');
 
-        expect(track[1].number).to.be(2);
-        expect(track[1].type).to.be('AUDIO');
+        expect(tracks[1].number).to.be(2);
+        expect(tracks[1].type).to.be('AUDIO');
     });
 
-    it('should parse track detail info', function() {
-        var tracks = sheet.files[0].tracks,
-        track1 = track[0],
-        track2 = track[1];
-
-        expect(track1.title).to.be('Sample track 1');
-        expect(track1.performer).to.be('Sample performer');
-        expect(track1.indexes).to.have.length(2);
-        expect(track1.indexes[0]).to.be('00:00:00');
-        expect(track1.indexes[1]).to.be('00:00:33');
-
-        expect(track2.title).to.be('Sample track 2');
-        expect(track2.performer).to.be('Sample performer');
-        expect(track2.indexes).to.have.length(2);
-        expect(track2.indexes[0]).to.be('00:05:10');
-        expect(track2.indexes[1]).to.be('00:05:23');        
+    it('should parse all REMs', function() {
+        expect(sheet.rem).to.be.an('array');
+        expect(sheet.rem[0]).to.be('Comment in toplevel');
+        expect(sheet.rem[1]).to.be('Comment in track');
     });
+
+    describe('track detail infos', function() {
+        var track1, track2;
+
+        beforeEach(function() {
+            var tracks = sheet.files[0].tracks;
+
+            track1 = tracks[0],
+            track2 = tracks[1];
+        });
+
+        it('should parse FLAGS', function() {
+            expect(track1.flags).to.be.an('array');
+            expect(track1.flags[0]).to.be('DCP');
+            expect(track1.flags[1]).to.be('PRE');
+        });
+
+        it('should parse track ISRC', function() {
+            expect(track1.isrc).to.be('ABCDE1234567');
+            expect(track2.isrc).to.be(null);
+        });
+
+        it('should parse track TITLE', function () {
+            expect(track1.title).to.be('Sample track 1');
+            expect(track2.title).to.be('Sample track 2');
+        });
+
+        it('should parse track PERFORMER', function() {
+            expect(track1.performer).to.be('Sample performer');
+            expect(track2.performer).to.be('Sample performer');
+        });
+
+        it('should parse track SONGWRITER', function() {
+            expect(track1.songWriter).to.be('Sample songwriter');
+            expect(track2.songWriter).to.be('Sample songwriter');
+        });
+
+        it('should parse track INDEX', function () {
+            expect(track1.indexes).to.have.length(2);
+            expect(track1.indexes[0].number).to.be(0);
+            expect(track1.indexes[0].time).to.be('00:00:00');
+            expect(track1.indexes[1].number).to.be(1);
+            expect(track1.indexes[1].time).to.be('00:00:33');
+
+            expect(track2.indexes).to.have.length(2);
+            expect(track2.indexes[0].number).to.be(0);
+            expect(track2.indexes[0].time).to.be('00:05:10');
+            expect(track2.indexes[1].number).to.be(1);
+            expect(track2.indexes[1].time).to.be('00:05:23');
+        });
+
+        it('should parse track PREGAP', function() {
+            expect(track1.pregap).to.be('00:02:00');
+            expect(track2.pregap).to.be(null);
+        });
+
+        it('should parse track POSTGAP', function() {
+            expect(track1.postgap).to.be('00:02:00');
+            expect(track2.postgap).to.be(null);
+        });
+    })
 });
