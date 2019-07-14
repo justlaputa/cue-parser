ecfb6569965c711e2ad7ec474fed6e6c174609c9
Borewit <borewit@users.noreply.github.com>
2019-07-14 15:39:12 +0200

    Keep Node v0.10 .. v4 compatibility

--- test/test.js
+++ test/test.js
@@ -153,14 +153,14 @@ describe('cue-parser', function() {
 
     describe('it should parse EAC generated CUE files', function() {
 
-        it('parse: Frank Boeijen - Palermo - CD1.eac.cue', () => {
+        it('parse: Frank Boeijen - Palermo - CD1.eac.cue', function() {
             sheet = parser.parse(path.join(__dirname, 'Frank Boeijen - Palermo - CD1.eac.cue'));
             expect(sheet.performer).to.be('Frank Boeijen');
             expect(sheet.title).to.be('Palermo');
             expect(sheet.files.length).to.be(10);
         });
 
-        it('parse: Frank Boeijen Groep - Welkom In Utopia', () => {
+        it('parse: Frank Boeijen Groep - Welkom In Utopia', function() {
             sheet = parser.parse(path.join(__dirname, 'Frank Boeijen Groep - Welkom In Utopia.eac.cue'));
             expect(sheet.performer).to.be('Frank Boeijen Groep');
             expect(sheet.title).to.be('Welkom In Utopia');
@@ -170,7 +170,7 @@ describe('cue-parser', function() {
 
     describe('it should parse XLD generated CUE files', function() {
 
-        it('parse: Putumayo Presents - Yoga Lounge', () => {
+        it('parse: Putumayo Presents - Yoga Lounge', function() {
             sheet = parser.parse(path.join(__dirname, 'Putumayo Presents - Yoga Lounge.xld.cue'));
             expect(sheet.files.length).to.be(12);
 
